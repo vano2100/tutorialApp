f96d44af8629c8dd492816c0f63aab210196c041
Rudenko Ivan <rudenko_ia@outlook.com>
2019-01-30 16:51:53 +0900

    GL 5.10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,21 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
-    @article.save
-    redirect_to @article
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end  
 end
