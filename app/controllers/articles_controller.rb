d6002df595b5dced3e036335308342f34971199e
Ivan Rudenko <rudenko_ia@outlook.com>
2019-02-03 18:16:24 +0900

    GL 5.11

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -21,8 +25,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 private
   def article_params
     params.require(:article).permit(:title, :text)
-  end  
+  end
 end
